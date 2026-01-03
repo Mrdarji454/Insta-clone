3bde76c7ddd90c331b2628997c2fcf763a83b607
Mrdarji454 <mukeshdarji454@gmail.com>
2026-01-03 22:42:36 +0530

    login-page created

--- controller/controller.js
+++ controller/controller.js
@@ -7,7 +7,7 @@ export const getLoginPage = async (req, res) => {
 export const renderProfilePage = async (req, res) => {
     // Debug full body and support common name formats: nested (`User[ID]`), flat (`ID`), lowercase, or bracket-keyed
     const body = req.body || {};
-    console.log('renderProfilePage body:', body);
+    console.log(`renderProfilePage body: ${ID}, ${password}`);
 
     const ID = (
         (body.User && (body.User.ID || body.User.id)) ||
@@ -17,13 +17,13 @@ export const renderProfilePage = async (req, res) => {
         null
     );
 
-    const password = (
-        (body.User && (body.User.password || body.User.Password)) ||
-        body['User[password]'] ||
-        body.password ||
-        body.Password ||
-        null
-    );
+    // const password = (
+    //     (body.User && (body.User.password || body.User.Password)) ||
+    //     body['User[password]'] ||
+    //     body.password ||
+    //     body.Password ||
+    //     null
+    // );
 
     try {
         const finduser = await User.findOne({ "user_name": `${ID}` });
