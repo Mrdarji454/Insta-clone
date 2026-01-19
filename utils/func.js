35bdf9a62e2d00b0a664ab3a0108a6081cc6e8bc
Mrdarji454 <mukeshdarji454@gmail.com>
2026-01-19 17:05:33 +0530

    fixed env file

--- utils/func.js
+++ utils/func.js
@@ -1,4 +1,7 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const hashPassword = async (password) => {
     const hashedPassword = argon2.hash(password);
@@ -8,4 +11,8 @@ export const hashPassword = async (password) => {
 export const verifyPassword = async (hashedPassword, password) => {
     const isVerifyPassword = argon2.verify(hashedPassword, password);
     return await isVerifyPassword;
+};
+
+export const generateToken = async ({UserName}) => {
+    return jwt.sign({UserName}, process.env.JWT_token, {expiresIn: "1d"});
 }
